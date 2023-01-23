21796770d1a82d7f15e7b5c63e2f2da4b52612ef
Abby <abby@Abbys-MacBook-Pro.local>
2023-01-23 17:18:19 -0500

    creates folders for each size

--- renamer.py
+++ renamer.py
@@ -1,3 +1,11 @@
 import os
 
-print("hellow world")
+os.chdir('/Users/abby/Embroidery Fonts/test font/')
+
+sizes=[1,2,3]
+for x in sizes:
+    if not "{} Inch".format(x) in os.listdir():
+        os.mkdir("{} Inch".format(x))
+
+for f in os.listdir():
+    print(f)
