6c269182a860704c1a3705eb8af002e8c494d349
AbbyGeek <abbyrosewest@gmail.com>
2023-01-24 23:40:17 -0500

    add flowers

--- renamer.py
+++ renamer.py
@@ -1,18 +1,23 @@
 import os
 
+# Change comp var to what computer I'm working on, or to appropriate folder
 PC = "C:\\Users\Abby\Desktop\Patches\Stock\Fonts\PES\PES\\test font"
 MAC = "/Users/abby/Embroidery Fonts/test font"
 comp = PC
 os.chdir(PC)
 
+# Check if font folder has additional PES folder and move into it
 if os.path.isdir('PES'):
     os.chdir(comp+"/pes")
+
+# Splits and extrapolates variables for sorting
 for f in os.listdir():
     fList = f.split()
     if(fList[-1]) == "Inch.pes":
         size = fList[-2]
         char = fList[-3].lower()
         charType = fList[-4].lower()
+        # Creates folder for specific size if not created
         if not os.path.isdir("{} Inch".format(size)):
             os.mkdir("{} Inch".format(size))
         #Upper
@@ -40,4 +45,5 @@ for f in os.listdir():
             destination = '{}/{} Inch/{}.pes'.format(comp, size, char)
         else: print("Error w/ file {}".format(f))
         oldFile = '{}/{}'.format(comp, f)
+        # Move and rename file
         os.rename(oldFile, destination)
